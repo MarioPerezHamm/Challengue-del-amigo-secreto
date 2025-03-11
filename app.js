23349fe1148623b863683e8150a9e075402b123a
Mario Hamming <perezfernan159@gmail.com>
2025-03-11 18:47:31 -0500

    ultima version del juego del amigo secreto

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación .
+let amigos= [];
 
+function agregarAmigo (){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
 
-function agregarAmigo(){
-    let nombre = document.getElementById('amigo').value;
-    amigos.push(nombre);
-    console.log(amigos);
-       
+    if(!nombreAmigo) {
+        alert('Por favor, ingrese un nombre de amigo.');
+        return;
+    }
+    // Alerta para que no se duplique el nombre de amigo 
+    if (amigos.includes(nombreAmigo)) {
+        alert('Este amigo ya está en la lista.');
+        return;
+    }
+    amigos.push(nombreAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    renderizarAmigos();
 }
 
+function renderizarAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for(let i = 0 ; i < amigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    }
+}
+    function sortearAmigos(){
+        if(amigos.length == 0){
+            alert('no hay amigos para sortear');
+            return;
+        }
+
+        // si solo se ecnuentra un amigo en la lista 
+        if (amigos.length == 1){
+            alert('Solo hay un amigo, no es necesario sortear.');
+            return;
+        }
+
+        let amigoSorteado = amigos [Math.floor(Math.random()* amigos.length)];
+        console.log(amigoSorteado);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = amigoSorteado ;
+
+
+    }
